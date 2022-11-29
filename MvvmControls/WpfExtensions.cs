2fa75ada476e9c8a4b1b81c41f1f1f3c4e39e5e8
RFBomb <robbery525@gmail.com>
2022-11-29 14:49:53 -0500

    Rename

---
+++ MvvmControls/WpfExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace System.Windows
+{
+    /// <summary>
+    /// Contains Extension Methods for Dependency Objects
+    /// </summary>
+    public static class WpfExtensions
+    {
+        private static readonly DependencyObject DesignModeTester = new();
+        
+        /// <summary>
+        /// Detect if in in the VS designer or not
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsDesignMode(this DependencyObject obj) => System.ComponentModel.DesignerProperties.GetIsInDesignMode(obj);
+    }
+}
