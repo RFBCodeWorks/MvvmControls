f9077f49d16b110f1dbab8aee0b00adfbfd80c50
RFBomb <20431767+RFBomb@users.noreply.github.com>
2023-03-27 19:33:10 -0400

    Update IRangeControl.cs

--- MvvmControls/Mvvm/IRangeControl.cs
+++ MvvmControls/Mvvm/IRangeControl.cs
@@ -4,9 +4,12 @@ using System.Windows.Controls.Primitives;
 namespace RFBCodeWorks.Mvvm
 {
     /// <summary>
-    /// Interface for the definition of a control that interacts with a <see cref="System.Windows.Controls.Primitives.RangeBase"/>
+    /// Interface for the definition of a control that interacts the following classes:
+    /// <br/> - <see cref="System.Windows.Controls.Primitives.RangeBase"/>
+    /// <br/> - <see cref="RFBCodeWorks.WPF.Controls.DecimalUpDown"/>
+    /// <br/> - <see cref="RFBCodeWorks.WPF.Controls.IntegerUpDown"/>
     /// </summary>
-    interface IRangeControl : IControlDefinition
+    public interface IRangeControl : IControlDefinition
     {
         /// <inheritdoc cref="RangeBase.ValueChanged"/>
         event EventHandler ValueChanged;
@@ -27,4 +30,28 @@ namespace RFBCodeWorks.Mvvm
         double Value { get; set; }
 
     }
+
+    ///// <summary>
+    ///// A strongly-typed version of the <see cref="IRangeControl"/> interface, that allows conversion to the other numeric values
+    ///// </summary>
+    ///// <typeparam name="T">The type of value (<see cref="int"/>, <see cref="double"/>, <see cref="long"/>, etc )</typeparam>
+    //public interface IRangeControl<T> : IRangeControl
+    //    where T: struct, IComparable<T>, IEquatable<T>, IFormattable
+    //{
+    //    /// <inheritdoc cref="RangeBase.Minimum"/>
+    //    new T Minimum { get; set; }
+
+    //    /// <inheritdoc cref="RangeBase.Maximum"/>
+    //    new T Maximum { get; set; }
+
+    //    /// <inheritdoc cref="RangeBase.SmallChange"/>
+    //    new T SmallChange { get; set; }
+
+    //    /// <inheritdoc cref="RangeBase.LargeChange"/>
+    //    new T LargeChange { get; set; }
+
+    //    /// <inheritdoc cref="RangeBase.Value"/>
+    //    new T Value { get; set; }
+    //}
+
 }
