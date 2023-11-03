30b106016f79a631ba2b1fce0ebccf63f22ecba9
RFBomb <20431767+RFBomb@users.noreply.github.com>
2023-11-03 10:23:57 -0400

    WebView2Integration
     - Provide support for WebView2

---
+++ WebView2Integration/NavigationHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFBCodeWorks.WPF.WebView2Integration
+{
+    /// <summary>
+    /// Class that implements <see cref="IWebView2NavigationHandler"/> via its delegate properties.
+    /// </summary>
+    public class NavigationHandler : IWebView2NavigationHandlerExpanded
+    {
+
+        /// <inheritdoc cref="IWebView2NavigationHandlerExpanded.OnBasicAuthenticationRequested"/>
+        public EventHandler<CoreWebView2BasicAuthenticationRequestedEventArgs> BasicAuthenticationRequestedHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnContentLoading"/>
+        public EventHandler<CoreWebView2ContentLoadingEventArgs> ContentLoadingHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnCoreWebView2InitializationCompleted"/>
+        public EventHandler<CoreWebView2InitializationCompletedEventArgs> CoreWebView2InitializationCompletedHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnHistoryChanged"/>
+        public EventHandler<object> HistoryChangedHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnNavigationCompleted"/>
+        public EventHandler<CoreWebView2NavigationCompletedEventArgs> NavigationCompletedHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnNavigationStarting"/>
+        public EventHandler<CoreWebView2NavigationStartingEventArgs> NavigationStartingHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnSourceChanged"/>
+        public EventHandler<CoreWebView2SourceChangedEventArgs> SourceChangedHandler { get; set; }
+
+        /// <inheritdoc cref="IWebView2NavigationHandler.OnWebMessageReceived"/>
+        public EventHandler<CoreWebView2WebMessageReceivedEventArgs> WebMessageReceivedHandler { get; set; }
+
+
+        public void OnBasicAuthenticationRequested(object sender, CoreWebView2BasicAuthenticationRequestedEventArgs e) => BasicAuthenticationRequestedHandler?.Invoke(sender, e);
+        public void OnContentLoading(object sender, CoreWebView2ContentLoadingEventArgs e) => ContentLoadingHandler?.Invoke(sender, e);
+        public void OnCoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e) => CoreWebView2InitializationCompletedHandler?.Invoke(sender, e);
+        public void OnHistoryChanged(object sender, object e) => HistoryChangedHandler?.Invoke(sender, e);
+        public void OnNavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e) => NavigationCompletedHandler?.Invoke(sender, e);
+        public void OnNavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e) => NavigationStartingHandler?.Invoke(sender, e);
+        public void OnSourceChanged(object sender, CoreWebView2SourceChangedEventArgs e) => SourceChangedHandler?.Invoke(sender, e);
+        public void OnWebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e) => WebMessageReceivedHandler?.Invoke(sender, e);
+    }
+}
---
+++ WebView2Integration/WebView2BindingHelper.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RFBCodeWorks.WPF.WebView2Integration
+{
+    public class WebView2BindingHelper : FrameworkElement
+    {
+        static WebView2BindingHelper()
+        {
+            IsVisibleProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(false));
+            IsHitTestVisibleProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(false));
+            VisibilityProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(Visibility.Collapsed));
+            HeightProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(0));
+            MaxHeightProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(0));
+            WidthProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(0));
+            MaxWidthProperty.OverrideMetadata(typeof(WebView2BindingHelper), new PropertyMetadata(0));
+        }
+
+        private bool IsNavigationHandlerExpanded;
+
+        public WebView2 WebView
+        {
+            get { return (WebView2)GetValue(WebViewProperty); }
+            set { SetValue(WebViewProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WebView.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WebViewProperty =
+            DependencyProperty.Register("WebView", typeof(WebView2), typeof(WebView2BindingHelper), new PropertyMetadata(null, WebViewChanged));
+
+        public IWebView2NavigationHandler NavigationHandler
+        {
+            get { return (IWebView2NavigationHandler)GetValue(NavigationHandlerProperty); }
+            set { SetValue(NavigationHandlerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NavigationHandler.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NavigationHandlerProperty =
+            DependencyProperty.Register(nameof(NavigationHandler), typeof(IWebView2NavigationHandler), typeof(WebView2BindingHelper), new PropertyMetadata(null, NavigationHandlerChanged));
+
+        private static void NavigationHandlerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is WebView2BindingHelper wv)
+            {
+                wv.IsNavigationHandlerExpanded = e.NewValue is IWebView2NavigationHandlerExpanded;
+            }
+        }
+
+        private static void WebViewChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not WebView2BindingHelper bh) return;
+            if (e.OldValue is WebView2 old)
+            {
+                //Basic
+                old.NavigationStarting -= bh.OnNavigationStarting;
+                old.NavigationCompleted -= bh.OnNavigationCompleted;
+                //Expanded
+                old.ContentLoading -= bh.OnContentLoading;
+                old.CoreWebView2InitializationCompleted -= bh.OnCoreWebView2InitializationCompleted;
+                old.CoreWebView2.HistoryChanged -= bh.CoreWebView2_HistoryChanged;
+                old.CoreWebView2.BasicAuthenticationRequested -= bh.CoreWebView2_BasicAuthenticationRequested;
+                old.SourceChanged -= bh.OnSourceChanged;
+                old.WebMessageReceived -= bh.OnWebMessageReceived;
+            }
+            if (e.NewValue is WebView2 wv)
+            {
+                //Basic
+                wv.NavigationStarting += bh.OnNavigationStarting;
+                wv.NavigationCompleted += bh.OnNavigationCompleted;
+                //Expanded
+                wv.ContentLoading += bh.OnContentLoading;
+                wv.CoreWebView2InitializationCompleted += bh.OnCoreWebView2InitializationCompleted;
+                wv.CoreWebView2.HistoryChanged += bh.CoreWebView2_HistoryChanged;
+                wv.CoreWebView2.BasicAuthenticationRequested += bh.CoreWebView2_BasicAuthenticationRequested;
+                wv.SourceChanged += bh.OnSourceChanged;
+                wv.WebMessageReceived += bh.OnWebMessageReceived;
+            }
+        }
+
+        private void CoreWebView2_BasicAuthenticationRequested(object sender, Microsoft.Web.WebView2.Core.CoreWebView2BasicAuthenticationRequestedEventArgs e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnBasicAuthenticationRequested(sender, e);
+        }
+
+        private void CoreWebView2_HistoryChanged(object sender, object e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnHistoryChanged(sender, e);
+        }
+
+        private void OnCoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnCoreWebView2InitializationCompleted(sender, e);
+        }
+
+        private void OnWebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnWebMessageReceived(sender, e);
+        }
+
+        private void OnSourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnSourceChanged(sender, e);
+        }
+
+        private void OnContentLoading(object sender, Microsoft.Web.WebView2.Core.CoreWebView2ContentLoadingEventArgs e)
+        {
+            if (IsNavigationHandlerExpanded && NavigationHandler is IWebView2NavigationHandlerExpanded nav)
+                nav.OnContentLoading(sender, e);
+        }
+
+        private void OnNavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            NavigationHandler?.OnNavigationStarting(sender, e);
+        }
+
+        private void OnNavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            NavigationHandler?.OnNavigationCompleted(sender, e);
+        }
+    }
+}
