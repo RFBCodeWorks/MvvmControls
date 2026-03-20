a14881d272301a9855ff543a2dc12b900d4442bd
RBrenckman <20431767+RFBomb@users.noreply.github.com>
2026-03-20 15:52:12 -0400

    Selector Generator Tests

---
+++ tests/MvvmControlsTests/Mvvm.SourceGenerators.Tests/GeneratorInputs/Selector.cs
@@ -0,0 +1,11 @@
+﻿namespace RFBCodeWorks.Mvvm.SourceGenerators.Tests.GeneratorInputs
+{
+    public partial class Selector
+    {
+        [ListBox]
+        [ComboBox]
+        [Selector]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0300:Simplify collection initialization")]
+        private int[] GetItems() => new int[] { 1, 2, 3, 4, 5 };
+    }
+}
